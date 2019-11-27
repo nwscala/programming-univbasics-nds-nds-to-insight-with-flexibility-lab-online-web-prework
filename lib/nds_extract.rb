d6321acc3f2c76754d802d8753c823d071ea8137
Noah Scala <tobi8912@gmail.com>
2019-11-27 02:07:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -79,6 +79,8 @@ def gross_per_studio(collection)
   studio_total = 0
   while index < collection.length do
     if !(total_hash.has_key?('collection[index][:studio]'))
+      
+    
   end 
   total_hash
 end
